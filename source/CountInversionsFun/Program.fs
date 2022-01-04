1a3959ca450d2d234f1ece14c53eec507e0bf196
Édgar Sánchez Gordón <edgar.yachakuq@hotmail.com>
2022-01-04 10:04:24 -0500

    MergeSort implemented

---
+++ source/CountInversionsFun/Program.fs
@@ -0,0 +1,52 @@
+﻿let mergeAndCountSplitInv c d =
+    let cLength = Array.length c
+    let dLength = Array.length d
+    let b = Array.zeroCreate (cLength + dLength)
+
+    let rec mergeCurrent i j k (splitInv: uint) =
+        let cItemsAvailable = i < cLength
+        let dItemsAvailable = j < dLength
+        if cItemsAvailable && (not dItemsAvailable || c[i] < d[j]) then
+            b[k] <- c[i]
+            mergeCurrent (i + 1) j (k + 1) splitInv
+        elif dItemsAvailable then
+            b[k] <- d[j]
+            mergeCurrent i (j + 1) (k + 1) 
+                (if cItemsAvailable then splitInv + uint (cLength - i) else splitInv)
+        else
+            b, splitInv
+
+    mergeCurrent 0 0 0 0u
+
+let rec sortAndCountInv a : int array * uint =
+    if Array.length a <= 1 then
+        a, 0u
+    else
+        let halfLength = Array.length a / 2
+        let c, leftInv = sortAndCountInv a[.. halfLength - 1]
+        let d, rightInv = sortAndCountInv a[halfLength ..]
+        let b, splitInv = mergeAndCountSplitInv c d
+        b, leftInv + rightInv + splitInv
+
+let testMergeSplitInv c d =
+    let b, splitInv = mergeAndCountSplitInv c d
+
+    printfn "c = %A" c
+    printfn "d = %A" d
+    printfn "b = %A" b
+    printfn "splitInv = %d" splitInv
+
+let testSplitInv a =
+    let b, splitInv = sortAndCountInv a
+
+    printfn "a = %A" a
+    printfn "b = %A" b
+    printfn "splitInv = %d" splitInv
+
+[<EntryPoint>]
+let main _ =
+    let numbers = [| 5; 3; 9; 2; 8; 1 |]
+    let b, splitInv = sortAndCountInv numbers
+    printfn "splitInv = %d" splitInv
+
+    0
--- source/MergeSortFun/Program.fs
+++ source/MergeSortFun/Program.fs
@@ -15,36 +15,41 @@
             j <- j + 1
     b
 
-let mergeWithRec c d =
+let merge c d =
     let cLength = Array.length c
     let dLength = Array.length d
-    let b = Array.zeroCreate (cLength + dLength)
+    let bLength = cLength + dLength
+    let b = Array.zeroCreate bLength
 
-    let rec mergeCurrent i j k =
-        if i < cLength && (j >= dLength || c[i] <= d[j]) then
-            b[k] <- c[i]
-            mergeCurrent (i + 1) j (k + 1)
-        elif j < dLength then
-            b[k] <- d[j]
-            mergeCurrent i (j + 1) (k + 1)
-        else
+    let rec mergeItem i j k =
+        if k >= bLength then
             b
+        elif i < cLength && (j >= dLength || c[i] <= d[j]) then
+            b[k] <- c[i]
+            mergeItem (i + 1) j (k + 1)
+        else
+            b[k] <- d[j]
+            mergeItem i (j + 1) (k + 1)
 
-    mergeCurrent 0 0 0
+    mergeItem 0 0 0
 
 let rec mergeSortWithLoop a =
     if Array.length a <= 1 then
         a
     else
         let halfLength = Array.length a / 2
-        mergeWithLoop (mergeSortWithLoop a[.. halfLength - 1]) (mergeSortWithLoop a[halfLength ..])
+        mergeWithLoop 
+            (mergeSortWithLoop a[.. halfLength - 1])
+            (mergeSortWithLoop a[halfLength ..])
 
-let rec mergeSortWithRec a =
+let rec mergeSort a =
     if Array.length a <= 1 then
         a
     else
         let halfLength = Array.length a / 2
-        mergeWithRec (mergeSortWithRec a[.. halfLength - 1]) (mergeSortWithRec a[halfLength ..])
+        let c = mergeSort a[.. halfLength - 1]
+        let d = mergeSort a[halfLength ..]
+        merge c d
 
 let quickBenchmark a sorter =
     let stopWatch = System.Diagnostics.Stopwatch()
@@ -52,15 +57,14 @@ let quickBenchmark a sorter =
     let b = sorter a
     stopWatch.Stop()
 
-    $"Time to sort: {stopWatch.ElapsedMilliseconds} ms"
-
+    $"Time to sort {Array.length b} items: {stopWatch.ElapsedMilliseconds} ms"
 
 [<EntryPoint>]
 let main _ =
     let rand = System.Random()
     let a = Array.init 5_000_000 (fun _ -> rand.Next())
 
-    printfn "Merge-Sort With loop - %s" (quickBenchmark a mergeSortWithLoop)
-    printfn "Merge-Sort With recursion - %s" (quickBenchmark a mergeSortWithRec)
+    printfn "Merge-Sort with loop - %s" (quickBenchmark a mergeSortWithLoop)
+    printfn "Merge-Sort with recursion - %s" (quickBenchmark a mergeSort)
 
     0
